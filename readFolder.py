cfe9eb30826091c03c171fba35593b3230c99f4d
Isabel Cenamor <39404410+icenamor@users.noreply.github.com>
2019-03-11 16:52:35 +0100

    Add files via upload

--- readFolder.py
+++ readFolder.py
@@ -25,13 +25,13 @@ def dir_list(dir_name, subdir, *args):
 def combine_files(fileList, fn):
     f = open(fn, 'w')
     for file in fileList:
-        print 'Writing file %s' % file
-		lines = open(file).readlines()
+        print ('Writing file %s' % file)
+        lines = open(file).readlines()
         f.write(lines[len(lines)-1]) ##read only the last 
     f.close()
  
 if __name__ == '__main__':
-    search_dir = "C:/directory"
-    fn = "output_file.arff"
-    combine_files(dir_list(search_dir, true, 'arff'), fn)
+    search_dir = 'C:\Users\Isabel\Downloads\results-SAT-2018.tar\results-SAT-2018'
+    fn = "salida.arff"
+    combine_files(dir_list(search_dir, true, 'output_file.arff'), fn)
 	
